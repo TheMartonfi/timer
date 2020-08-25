83630e89f012714a1e7e696ba88d9ac9d36ef1a5
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-25 20:59:27 +0000

    Created a program that takes user inputs in seconds and sets an alarm for every input they provided

---
+++ timer1.js
@@ -0,0 +1,36 @@
+
+const getUserInput = () => {
+  return process.argv.splice(2);
+};
+
+
+const validateUserInputs = (userInput) => {
+  
+  let validInputs = [];
+  
+  for (const input of userInput) {
+
+    const userInputToNum = Number(input)
+
+    if (!isNaN(userInputToNum) && Math.sign(userInputToNum) === 1 && Number.isInteger(userInputToNum) && userInputToNum > 0) {
+      validInputs.push(userInputToNum);
+    }
+  }
+  
+  return validInputs;
+};
+
+const setAlarm = (userInput) => {
+  
+  if (userInput.length <= 0) {
+    return null;
+  } else {
+    for (const input of userInput) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, input * 1000)
+    }
+  }
+};
+
+setAlarm(validateUserInputs(getUserInput()));
